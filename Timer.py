56733f7ad69c9909258f97fc99bd7fe24862b7c1
Olson <976308589@qq.com>
2019-06-11 13:17:17 +0800

    每天更新一次

--- Timer.py
+++ Timer.py
@@ -4,13 +4,19 @@ import PySimpleGUI as sg
 import arrow as ar
 import requests as req
 
-res = req.get('https://www.baidu.com/').headers['Date']
-time_diff = ar.get(res[4:-4], 'DD MMM YYYY HH:mm:ss') - ar.now().floor('second')
-
 layout = [[sg.Text('', size=(10, 2), font=('Comic Sans MS', 20), justification='center', key='Timer')]]
 
-window = sg.Window('Timer', layout)
+window = sg.Window('TIME', layout)
+
+count = 0
 while True:
+    if count == 0:
+        res = req.get('https://www.baidu.com/').headers['Date']
+        time_diff = ar.get(res[4:-4], 'DD MMM YYYY HH:mm:ss') - ar.now().floor('second')
+    elif count > 10 * 3600 * 24:
+        count = 0
+    else:
+        count += 1
     event, values = window.Read(timeout=10)  # Please try and use a timeout when possible
     if event is None or event == 'Exit':
         break
